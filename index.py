7bfe18391fe6493eb877c6b2a7c4bab499387671
Shpm21 <galleguilloscristian2@gmail.com>
2022-12-18 13:20:06 -0300

    change Name to Endpoint

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
-from app.common.Image import Image
+from app.common.Image import ImageEndPoint
 from flask_cors import CORS
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-api.add_resource(Image, '/image/')
+api.add_resource(ImageEndPoint, '/image/')
 
 
 if __name__ == '__main__':
