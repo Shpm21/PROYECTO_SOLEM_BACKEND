18530c47517786272a6cb1635e9b6fe37191c1fc
Emilio Rojas Veliz <rovel.code@outlook.com>
2022-12-27 16:30:25 -0300

    features(server image processing): add histogram display and ecualization of images

--- server.py
+++ server.py
@@ -1,39 +1,62 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import sys
-# THIS IS NEEDED TO IMPORT "processRequestImage" WHICH IS STORED IN ANOTHER DIRECTORY
+# THIS IS NEEDED TO IMPORT "ImageProcessor" WHICH IS STORED IN ANOTHER DIRECTORY
 sys.path.append('./cvf')
-from imageProcessing import getTextFromSampleImage, getTextFromImageBytes
+# from ImageProcessor import getTextFromSampleImage, getTextFromImageBytes
+from ImageProcessor import ImageProcessor
 
-# getTextFromSampleImage()
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['GET'])
 @cross_origin()
-def httpGetTextFromSampleImage():
-    return getTextFromSampleImage()
-    # import io
-    # import requests
-    # from PIL import Image
-    # r = requests.get('https://www.bimbaylola.com/media/catalog/product/1/8/182BAC104_T2200_P_T_XX_1.jpg', stream=True)
-    # print(r)
-    # print(type(r))
-    # return "chaoooooooooooooooo"
-
+def httpGetTextFromSampleImage():    
+    return ImageProcessor.getTextFromSampleImage()
 
 @app.route("/images", methods=['POST'])
 @cross_origin()
-def httpImagesRepo():
-    
+def httpGetTextFromUploadedImage():
     if (request.files):
         imagefile = request.files['imagefile']
         imgBytes = imagefile.stream.read()
-        return getTextFromImageBytes(imgBytes)
+        return ImageProcessor.getTextFromImageBytes(imgBytes)
     else:
         return "Por favor, envía una imagen"
 
+
+@app.route("/enhanced_images_v1", methods=['POST'])
+@cross_origin()
+def httpGetTextFromUploadedImageEcualized_v1():
+    if (request.files):
+        imagefile = request.files['imagefile']
+        imgBytes = imagefile.stream.read()
+        return ImageProcessor.getTextFromImageBytesEcualized_v1(imgBytes)
+    else:
+        return "Por favor, envía una imagen"
+
+
+@app.route("/enhanced_images_v2", methods=['POST'])
+@cross_origin()
+def httpGetTextFromUploadedImageEcualized_v2():
+    if (request.files):
+        imagefile = request.files['imagefile']
+        imgBytes = imagefile.stream.read()
+        return ImageProcessor.getTextFromImageBytesEcualized_v2(imgBytes)
+    else:
+        return "Por favor, envía una imagen"
+
+
+@app.route("/testHist", methods=['POST'])
+@cross_origin()
+def httpTestHistogramEqualization():
+    if (request.files):
+        imagefile = request.files['imagefile']
+        imgBytes = imagefile.stream.read()
+        return ImageProcessor.showHistFromImgBytes(imgBytes)
+    else:
+        return "Por favor, envía una imagen"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
