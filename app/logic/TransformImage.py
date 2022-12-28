574c53403679feeb553238cc631e9fbfa55f4537
Shpm21 <galleguilloscristian2@gmail.com>
2022-12-28 14:31:19 -0300

    refactor methods

--- app/logic/TransformImage.py
+++ app/logic/TransformImage.py
@@ -1,6 +1,7 @@
 import base64
+
 import cv2
-import base64
+import numpy as np
 
 
 def get_base64_image(path: str):
@@ -12,6 +13,7 @@ def get_base64_image(path: str):
 
 class TransformImage:
     __instance = None
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     images_to_show = []  # the format is \
     # [{'image': image,
     #  'description': text },
@@ -34,23 +36,18 @@ class TransformImage:
         else:
             TransformImage.__instance = self
 
-    def get_encode_from_metadata(self, metadata):
-        return metadata.split(',')[1]
-
     def get_and_save_image_rgb(self, image):
-        image_64_decode = base64.b64decode(image)
-        image_result = open('deer_decode.jpg', 'wb')
-        image_result.write(image_64_decode)
-        img = cv2.imread('./deer_decode.jpg')
+        """Get the image in RGB format and save it"""
+        img = self.transform_image_to_cv2(image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cv2.imwrite('deer_decode.jpg', img)
-        self.append_image_to_show(
-            get_base64_image('deer_decode.jpg'),
+        return self.save_and_return_image(
+            img,
+            'deer_decode.jpg',
             'RGB'
         )
-        return cv2.imread('deer_decode.jpg')
 
     def get_and_save_image_grey(self, image):
+        """Get the image in grey format and save it"""
         img = self.get_and_save_image_rgb(image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return self.save_and_return_image(
@@ -60,9 +57,9 @@ class TransformImage:
         )
 
     def get_and_save_image_grey_equalize(self, image):
+        """Get the image in grey equalize format and save it"""
         img = self.get_and_save_image_grey(image)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        img = clahe.apply(img)
+        img = self.clahe.apply(img)
         return self.save_and_return_image(
             img,
             'deer_decode_grey_equalize.jpg',
@@ -70,6 +67,7 @@ class TransformImage:
         )
 
     def get_and_save_image_grey_equalize_binarize(self, image):
+        """Get the image in grey equalize binarize format and save it"""
         img = self.get_and_save_image_grey_equalize(image)
         _, img = cv2.threshold(
             img, 127, 255, cv2.THRESH_BINARY)
@@ -83,18 +81,34 @@ class TransformImage:
         return self.images_to_show
 
     def append_image_to_show(self, image: str, description: str):
+        """Append the image to show in the view"""
         self.images_to_show.append({
             'image': image,
             'description': description,
         })
 
     def save_and_return_image(self, image, path, description):
+        """Save the image and return it"""
         cv2.imwrite(path, image)
         self.append_image_to_show(
             get_base64_image(path),
             description
         )
-        return cv2.imread(path, 0)
+        return image
 
     def clear_images_to_show(self):
+        """Clear the images to show"""
         self.images_to_show = []
+
+    def transform_image_to_cv2(self, image):
+        """Transform the image to cv2 format"""
+        npimg = np.fromstring(image, np.uint8)
+        return cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    def get_encode_from_metadata(self, metadata):
+        """Get the encode from the metadata"""
+        return metadata.split(',')[1]
+
+    def decode_image(self, image):
+        """Decode the image"""
+        return base64.b64decode(image)
